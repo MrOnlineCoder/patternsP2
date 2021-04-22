7cc70fbd8dff7b00eae50e512b3a91eb6d8cb8eb
Anrashko <yurii.andrashko@uzhnu.edu.ua>
2021-04-22 18:19:19 +0300

    flyweight

--- cs/Program.cs
+++ cs/Program.cs
@@ -6,7 +6,7 @@ namespace cs
     {
         static void Main(string[] args)
         {
-            StructuralPaterns.TestBridge();
+            StructuralPaterns.TestFlyweight();
             Console.ReadLine();
         }
     }
